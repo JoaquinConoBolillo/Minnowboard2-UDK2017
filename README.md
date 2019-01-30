db2407556da0fca9ec9185bd885682dbf255ea4b
JC <Joaquin_ConoBolillo@yahoo.es>
2019-01-30 15:48:10 +0100

    ### 20190130/2 * integrate "Release 1.00 Firmware Download(Nov 2018)" https://firmware.intel.com/sites/default/files/minnowboard_max-1.00-binary.objects.zip * enable VS2015 build only

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 Build environment for the MinnowBoard
 
 # Revision histrory
+### 20190130/2
+* integrate "Release 1.00 Firmware Download(Nov 2018)" https://firmware.intel.com/sites/default/files/minnowboard_max-1.00-binary.objects.zip
+* enable VS2015 build only
 ### 20190130
 * integrate all source files into a VS2017 solution
 * (NOTE: use traditional EDK build, no VS2017 build)
--- launch.bat
+++ launch.bat
@@ -1,19 +1,5 @@
 @echo off
 goto START
-rem set DDKDIR=C:\WinDDK\3790~1.183
-rem ::set DDKDIR=C:\WinDDK\760016~1.1
-rem call !DDKDIR!\bin\setenv.bat !DDKDIR! fre WNET
-rem ::call C:\WinDDK\7600.16385.1\bin\setenv.bat C:\WinDDK\7600.16385.1\ fre x86 WXP
-rem pause
-rem cd %~dp0
-rem rem set LIB=%LIB%;!DDKDIR!\lib\wnet\i386;!DDKDIR!\lib\crt\i386
-rem set LIB=%LIB%;!DDKDIR!\lib\win7\i386;!DDKDIR!\lib\Crt\i386;!DDKDIR!\lib\wxp\i386
-rem SET INCLUDE=%INCLUDE%;!DDKDIR!\inc\crt
-
-::set DDKDIR=C:\WinDDK\3790~1.183
-set DDKDIR=C:\WinDDK\760016~1.1
-call !DDKDIR!\bin\setenv.bat !DDKDIR! fre WXP
-::call C:\WinDDK\7600.16385.1\bin\setenv.bat C:\WinDDK\7600.16385.1\ fre x86 WXP
 
 :START
 cd %~dp0
@@ -24,6 +10,9 @@ SET INCLUDE=%INCLUDE%;!DDKDIR!\inc\crt
 call edksetup.bat --nt32
 set OPENSSL_PATH=%WORKSPACE%\openssl-20170801
 set path=%path%;c:\bin;%~dp0CdePkg\tools;C:\PROGRA~2\NASM
+rem allow build by VS2015 only
+set VS110COMNTOOLS=
+set VS120COMNTOOLS=
 echo BUILD:             "build -t VS2015x86"
 echo RUN DEBUG BUILD:   "DBG"
 echo RUN RELEASE BUILD: "RUN"
